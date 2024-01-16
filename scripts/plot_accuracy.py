0d8c11e796ed52c6e55788f3c553bab30c284998
neworderofjamie <neworderofjamie@gmail.com>
2024-01-16 18:13:20 +0000

    seperate training and validation accuracy figures

--- scripts/plot_accuracy.py
+++ scripts/plot_accuracy.py
@@ -13,13 +13,50 @@ from itertools import product
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from pandas import concat, read_csv
 
+def plot_accuracy(fig, axis, df, accuracy_column):
+    # Get unique quantisation levels
+    quant_levels = df["quant_level"].unique()
+
+    # Build map of surrogate gradient + quantization algorithm permutations
+    pal = sns.color_palette()
+    colours = {q: pal[i] for i, q in enumerate((df["surrogate_gradient"] + "-" + df["quant_algo"]).unique())}
+
+    x = 0.0
+    tick_x = []
+    tick_label = []
+    for q in quant_levels:
+        if np.isnan(q):
+            label="None"
+            q_df = df[df["quant_level"].isna()]
+        else:
+            label = f"{q}\nlevels"
+            q_df = df[df["quant_level"] == q]
+
+        # Calculate bar padding
+        bar_x = x + (np.arange(len(q_df)) * BAR_PAD)
+        x = bar_x[-1] + GROUP_PAD
+
+        # Add label and
+        tick_label.append(label)
+        tick_x.append(np.average(bar_x))
+
+        # Plot bars
+        bar_colours = [colours[q] for q in (q_df["surrogate_gradient"] + "-" + q_df["quant_algo"]).to_list()]
+        axis.bar(bar_x, q_df[accuracy_column],# yerr=df["sd_train_accuracy"],
+                 color=bar_colours, width=BAR_WIDTH)
+    axis.set_xticks(tick_x, tick_label)
+    axis.set_xlabel("Error quantisation")
+    axis.set_ylim((0, 100))
+    fig.legend([patches.Patch(color=c) for c in colours.values()], colours.keys(),
+               loc="lower center", ncol=len(colours), frameon=False)
 
 BAR_WIDTH = 1.0
 BAR_PAD = 1.1
 GROUP_PAD = 2.5
 
 # Dictionary to hold data
-data = {"surrogate_gradient": [], "quant_level": [], "quant_algo": [], "seed": [], "test_accuracy": [], "train_accuracy": []}
+data = {"surrogate_gradient": [], "quant_level": [], "quant_algo": [],
+        "seed": [], "test_accuracy": [], "train_accuracy": []}
 
 # Loop through test files (signifies complete experiments)
 # **TODO** proper path
@@ -52,12 +89,13 @@ for name in glob(os.path.join("..", "classifier", "test_output_*.csv")):
     assert last_epoch_train_data.shape[0] == 1
 
     # Add data to intermediate dictionary
-    data["surrogate_gradient"].append(surrogate_gradient)
-    data["quant_level"].append(quant_level)
-    data["quant_algo"].append(quant_algo)
-    data["seed"].append(int(name_components[8]))
-    data["test_accuracy"].append((100.0 * (last_epoch_test_data["Number correct"] / last_epoch_test_data["Num trials"])).iloc[0])
-    data["train_accuracy"].append((100.0 * (last_epoch_train_data["Number correct"] / last_epoch_train_data["Num trials"])).iloc[0])
+    if name_components[7] == "mnist":
+        data["surrogate_gradient"].append(surrogate_gradient)
+        data["quant_level"].append(quant_level)
+        data["quant_algo"].append(quant_algo)
+        data["seed"].append(int(name_components[8]))
+        data["test_accuracy"].append((100.0 * (last_epoch_test_data["Number correct"] / last_epoch_test_data["Num trials"])).iloc[0])
+        data["train_accuracy"].append((100.0 * (last_epoch_train_data["Number correct"] / last_epoch_train_data["Num trials"])).iloc[0])
 
 # Build dataframe from dictionary and sort by config
 df = DataFrame(data=data)
@@ -71,48 +109,21 @@ df = df.agg(mean_test_accuracy=NamedAgg(column="test_accuracy", aggfunc=np.mean)
 
 print(f"{len(df)} configurations")
 
-# Get unique quantisation levels
-quant_levels = df["quant_level"].unique()
 
-pal = sns.color_palette()
+# Plot training accuracy
+train_quant_fig, train_quant_axis = plt.subplots(figsize=(plot_settings.double_column_width, 2.0))
+plot_accuracy(train_quant_fig, train_quant_axis, df, "mean_train_accuracy")
+train_quant_axis.set_ylabel("Training accuracy [%]")
+train_quant_fig.tight_layout(pad=0, rect=[0.0, 0.15, 1.0, 1.0])
 
-# Build map of surrogate gradient + quantization algorithm permutations
-colours = {q: pal[i] for i, q in enumerate((df["surrogate_gradient"] + "-" + df["quant_algo"]).unique())}
+# Plot validation accuracy
+valid_quant_fig, valid_quant_axis = plt.subplots(figsize=(plot_settings.double_column_width, 2.0))
+plot_accuracy(valid_quant_fig, valid_quant_axis, df, "mean_test_accuracy")
+valid_quant_axis.set_ylabel("Validation accuracy [%]")
+valid_quant_fig.tight_layout(pad=0, rect=[0.0, 0.15, 1.0, 1.0])
 
-
-quant_fig, quant_axis = plt.subplots(figsize=(plot_settings.double_column_width, 2.0))
-x = 0.0
-tick_x = []
-tick_label = []
-for q in quant_levels:
-    if np.isnan(q):
-        label="None"
-        q_df = df[df["quant_level"].isna()]
-    else:
-        label = f"{q}\nlevels"
-        q_df = df[df["quant_level"] == q]
-
-    # Calculate bar padding
-    bar_x = x + (np.arange(len(q_df)) * BAR_PAD)
-    x = bar_x[-1] + GROUP_PAD
-
-    # Add label and
-    tick_label.append(label)
-    tick_x.append(np.average(bar_x))
-
-    # Plot bars
-    bar_colours = [colours[q] for q in (q_df["surrogate_gradient"] + "-" + q_df["quant_algo"]).to_list()]
-    quant_axis.bar(bar_x, q_df["mean_test_accuracy"],# yerr=df["sd_train_accuracy"],
-                   color=bar_colours, width=BAR_WIDTH)
-quant_axis.set_xticks(tick_x, tick_label)
-quant_axis.set_xlabel("Error quantisation")
-quant_axis.set_ylabel("Validation accuracy [%]")
-quant_fig.legend([patches.Patch(color=c) for c in colours.values()], colours.keys(),
-                 loc="lower center", ncol=len(colours), frameon=False)
-quant_fig.tight_layout(pad=0, rect=[0.0, 0.15, 1.0, 1.0])
-#if not plot_settings.presentation and not plot_settings.poster:
-#    dense_fig.savefig("../figures/dense_accuracy.pdf")
-#    one_layer_sparse_fig.savefig("../figures/sparse_accuracy.pdf")
-#    two_layer_sparse_fig.savefig("../figures/two_layer_sparse_accuracy.pdf")
-#    best_accuracy_fig.savefig("../figures/best_accuracy.pdf")
+# Save figures
+if not plot_settings.presentation and not plot_settings.poster:
+    train_quant_fig.savefig("../figures/mnist_train_accuracy.pdf")
+    valid_quant_fig.savefig("../figures/mnist_valid_accuracy.pdf")
 plt.show()
